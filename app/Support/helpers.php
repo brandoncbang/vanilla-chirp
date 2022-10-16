646240d0b2ed596608fbc016fa20cc2ee765b104
Brandon Bang <36747510+brandoncbang@users.noreply.github.com>
2022-10-16 16:49:08 -0400

    Add HTML helpers

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -13,10 +13,40 @@ function redirect($to)
     exit;
 }
 
+function request_path(): string
+{
+    return rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
+}
+
 function param_path($signature): string|false
 {
-//    rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
+    $request_path = request_path();
+    $pattern = preg_replace('/{(.+?)}/', '(?<$1>[^/]+?)', $signature);
+    $regex = "@^{$pattern}$@";
+
+    if (preg_match($regex, $request_path)) {
+        return $request_path;
+    }
+
     return false;
+}
+
+function path_params($signature): array
+{
+    $request_path = request_path();
+    $pattern = preg_replace('/{(.+?)}/', '(?<$1>[^/]+?)', $signature);
+    $regex = "@^{$pattern}$@";
+    $params = [];
+
+    preg_match_all($regex, $request_path, $matches);
+
+    foreach ($matches as $key => $value) {
+        if (is_string($key)) {
+            $params[] = $value[0];
+        }
+    }
+
+    return $params;
 }
 
 function flash($key, $data)
@@ -67,6 +97,29 @@ function view($path, $data = [])
     extract($data);
     require __DIR__ . "/../../views/{$path}.php";
     exit;
+}
+
+function link_to(string|object $link, string $text)
+{
+    if (method_exists($link, 'getPath')) {
+        $link = $link->getPath();
+    }
+
+    return <<<HTML
+        <a href="{$link}">{$text}</a>
+        HTML;
+}
+
+function delete_button($action = '', $method = 'POST'): string
+{
+    return <<<HTML
+        <form action="{$action}" method="{$method}" onclick="return confirm('Are you sure?')"
+              style="display: inline-block">
+            <!-- TODO: Add CSRF token! -->
+
+            <button type="submit">Delete</button>
+        </form>
+        HTML;
 }
 
 function config(): array
