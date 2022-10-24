7162bb289e45f369853e73d303f8eb055274a6d6
Brandon Bang <36747510+brandoncbang@users.noreply.github.com>
2022-10-24 17:25:04 -0400

    Check for valid parent chirp

--- app/Validation/StoreChirpValidator.php
+++ app/Validation/StoreChirpValidator.php
@@ -2,11 +2,17 @@
 
 namespace App\Validation;
 
+use App\Models\Chirp;
+
 class StoreChirpValidator extends Validator
 {
     public function getErrors($input): array
     {
         $errors = [];
+
+        if (isset($input['chirp_id']) && !Chirp::find($input['chirp_id'])) {
+            $errors['chirp_id'] = "Can't reply to Chirp.";
+        }
 
         if (empty($input['content'])) {
             $errors['content'] = 'A Chirp must not be empty.';
